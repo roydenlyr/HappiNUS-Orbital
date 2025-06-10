08ec05513dcebb842a44ad96d76f9592abb70bb1
Royden <roydenlyr@gmail.com>
2025-06-11 03:13:36 +0800

    Added loading state for rephrase and summary feature to prevent multiple api calls

--- components/ChatRoomHeader.js
+++ components/ChatRoomHeader.js
@@ -1,16 +1,21 @@
 import { View, Text, TouchableOpacity, Pressable, Alert } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Stack } from 'expo-router'
 import { Entypo, Feather, FontAwesome, Ionicons } from '@expo/vector-icons'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { Image } from 'expo-image';
 import { summariseChat } from '../services/summariseChat';
 import { rephraseMessage } from '../services/rephraseMessage';
+import Loading from './Loading';
 
 const ChatRoomHeader = ({user, router, messages, textRef, inputRef}) => {
 
+    const [summaryLoading, setSummaryLoading] = useState(false);
+    const [rephraseLoading, setRephraseLoading] = useState(false);
+
     const handleSummary = async () => {
         if (messages){
+            setSummaryLoading(true);
             try {
                 const summary = await summariseChat(messages, user.userId);
                 console.log('Summary: ', summary);
@@ -18,14 +23,17 @@ const ChatRoomHeader = ({user, router, messages, textRef, inputRef}) => {
             } catch (error) {
                 console.error('Failed to summarise: ', error.message);
                 Alert.alert('Error', 'Failed to summarise chat. Please try again.');
+            } finally {
+                setSummaryLoading(false);
             }
         } else{
-
+            Alert.alert('Summary Failed', 'No messages to summarise');
         }
     }
 
     const handleRephrase = async () => {
-        if (textRef){
+        if (textRef.current && textRef.current.trim() !== ''){
+            setRephraseLoading(true);
             try {
                 const rephrased = await rephraseMessage(textRef.current);
                 console.log('Rephrased text: ', rephrased);
@@ -48,9 +56,11 @@ const ChatRoomHeader = ({user, router, messages, textRef, inputRef}) => {
             } catch (error) {
                 console.error('Failed to rephrase: ', error.message);
                 Alert.alert('Error', 'Failed to rephrase text. Please try again.');
+            } finally {
+                setRephraseLoading(false);
             }
         } else{
-            Alert.alert('Rephrasing Failed', 'Text box is empty');
+            Alert.alert('Rephrasing Failed', 'Please type a message first.');
         }
     }
 
@@ -76,10 +86,22 @@ const ChatRoomHeader = ({user, router, messages, textRef, inputRef}) => {
             headerRight: user?.role === 'student' ? () => (
                 <View className='flex-row items-center gap-8'>
                     <Pressable onPress={handleRephrase}>
-                        <FontAwesome name='stack-exchange' size={hp(2.8)} color={'#737373'}/>
+                        {
+                            rephraseLoading ? (
+                                <Loading size={hp(5)}/>
+                            ) : (
+                                <FontAwesome name='stack-exchange' size={hp(2.8)} color={'#737373'}/>
+                            )
+                        }
                     </Pressable>
-                    <Pressable onPress={handleSummary}>
-                        <Feather name='clipboard' size={hp(2.8)} color={'#737373'}/>
+                    <Pressable onPress={handleSummary} disabled={summaryLoading}>
+                        {
+                            summaryLoading ? (
+                                <Loading size={hp(5)}/>
+                            ) : (
+                                <Feather name='clipboard' size={hp(2.8)} color={'#737373'}/>
+                            )
+                        }
                     </Pressable>
                 </View>
             ) : undefined
