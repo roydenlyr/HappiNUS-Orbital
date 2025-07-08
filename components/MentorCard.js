bcf08f53a09deb16fabcaf8c6e59e91982d36f55
YuJie <yujie2164@gmail.com>
2025-07-08 17:46:16 +0800

    Update MentorCard UI layout and styling

--- components/MentorCard.js
+++ components/MentorCard.js
@@ -1,8 +1,8 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 const MentorCard = ({mentor}) => {
   // Calculating by academic year
@@ -19,42 +19,42 @@ const MentorCard = ({mentor}) => {
   const router = useRouter();
 
   const handleChatNow = () => {
-    router.replace({pathname: '/chatRoom', params: {
-      ...mentor,
-      profileUrl: encodeURIComponent(mentor.profileUrl)
-    }});    
+    router.replace({pathname: '/chatRoom', params: mentor});    
   }
 
   
   return (
-    <View style={{borderRadius: 40, height: '100%', width: wp(70), maxWidth: 600}} className='bg-indigo-500 self-center'>
-      <View style={{shadowColor: 'black', shadowRadius: 10, shadowOffset: {width: 0, height: 10}, shadowOpacity: 0.5}} className='flex-row justify-center -mt-14'>
-        <Image source={{uri: mentor.profileUrl}} style={{borderRadius: 100}} className='h-40 w-40' />
+    //  <View style={{borderRadius: 40, height: '100%', width: wp(70), maxWidth: 600}} className='bg-indigo-500 self-center'>
+<LinearGradient
+  colors={['#667eea', '#764ba2']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}
+  style={{ borderRadius: 40, height: '100%', width: wp(70), maxWidth: 600, alignSelf: 'center', paddingTop: hp(8)}}
+>
+      <View style={{shadowColor: 'black', shadowRadius: 40, shadowOffset: {width: 0, height: 30}, shadowOpacity: 0.7}} className='flex-row justify-center -mt-14'>
+        <Image source={{uri: mentor.profileUrl}}  className='h-40 w-40 rounded-full' />
       </View>
-
       <View className='flex-1 justify-center px-5'>
-        <View className='flex-row gap-4'>
-          <FontAwesome name='user-o' color={'white'} size={hp(3)}/>
-          <Text style={{fontSize: hp(3)}} className='font-semibold text-white'>{mentor.username}</Text>
+        <View style={{marginTop: hp(2), alignItems: 'center'}}>
+          {/* <FontAwesome name='user-o' color={'white'} size={hp(3)}/> */}
+          <Text style={{fontSize: hp(2), fontSize: hp(4)}} className='font-bold text-white'>{mentor.username}</Text>
         </View>
-        <View style={{marginTop: hp(2)}} className='flex-row gap-4'>
+        <View style={{marginTop: hp(2), paddingLeft: hp(2)}} className='flex-row gap-4'>
           <Ionicons name={genderIcon} color={'white'} size={hp(3)}/>
-          <Text style={{fontSize: hp(3)}} className='font-semibold text-white'>{mentor.gender}</Text>
+          <Text style={{fontSize: hp(2)}} className='text-white'>{mentor.gender}</Text>
         </View>
-        <View style={{marginTop: hp(2)}} className='flex-row gap-4'>
+        <View style={{marginTop: hp(2), paddingLeft: hp(2)}} className='flex-row gap-4'>
           <FontAwesome name='birthday-cake' color={'white'} size={hp(3)}/>
-          <Text style={{fontSize: hp(3)}} className='font-semibold text-white'>{age}</Text>
+          <Text style={{fontSize: hp(2)}} className='text-white'>{age}</Text>
         </View>
-        <View style={{marginTop: hp(2)}} className='flex-row gap-4'>
+        <View style={{marginTop: hp(2), paddingLeft: hp(2)}} className='flex-row gap-4'>
           <FontAwesome name='calendar' color={'white'} size={hp(3)}/>
-          <Text style={{fontSize: hp(3)}} className='font-semibold text-white'>Year {year}</Text>
+          <Text style={{fontSize: hp(2)}} className='text-white'>Year {year}</Text>
         </View>
-        <View style={{marginTop: hp(2)}} className='flex-row gap-4'>
+        <View style={{marginTop: hp(2), paddingLeft: hp(2), flexDirection: 'row', alignItems: 'center'}} className='flex-row gap-4'>
           <Ionicons name='school-sharp' color={'white'} size={hp(3)}/>
-          <Text style={{fontSize: hp(3)}} className='font-semibold text-white pr-10'>{mentor.faculty}</Text>
+          <Text style={{fontSize: hp(2)}} className='text-white pr-10' numberOfLines={2}>{mentor.faculty}</Text>
         </View>
       </View>
-      <View style={{marginTop: hp(2)}} className='flex-row gap-4 justify-end m-3'>
+      <View style={{marginTop: hp(2), paddingRight: hp(2), paddingBottom: hp(1)}} className='flex-row gap-4 justify-end m-3'>
         <TouchableOpacity onPress={handleChatNow} style={{height: hp(4)}} className='bg-white rounded-full items-center flex-row gap-3 px-4'>
           <Text style={{fontSize: hp(2)}} className='font-semibold text-indigo-500'>
             Need to talk?
@@ -62,8 +62,7 @@ const MentorCard = ({mentor}) => {
           <Ionicons name='chatbubble-outline' size={hp(3)} color={'#6366F1'}/>
         </TouchableOpacity>
       </View>
-      
-    </View>
+    </LinearGradient>
   )
 }
 
